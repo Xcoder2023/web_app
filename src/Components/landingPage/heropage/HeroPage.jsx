4cca97639bc5351a5c6433080dd2d2da325e9b46
adebabs-tec <ruqoyatomotayo@gmail.com>
2025-01-10 09:27:52 +0100

    new commit

--- src/Components/landingPage/heropage/HeroPage.jsx
+++ src/Components/landingPage/heropage/HeroPage.jsx
@@ -1,4 +1,4 @@
-import { IoMdArrowForward } from "react-icons/io";
+ew-ruiopmport { IoMdArrowForward } from "react-icons/io";
 
 export const HeroPage = () => {
   return (
@@ -9,19 +9,19 @@ export const HeroPage = () => {
         <div className="absolute inset-0 bg-[#0D183A] opacity-45"></div>
 
         {/* Content */}
-        <div className="relative xl:w-[60%] mx-auto flex flex-col items-center gap-5">
+        <div className="relative xl:w-[60%] mx-auto flex flex-col items-center gap-5  w-[90%]">
           <p className="text-center  xl:text-[48px] text-[28px]">
             <span className="">E</span>nsuring Every Child&apos;s Health &
             Safety in Schools
           </p>
-          <p className="text-[16px] text-center w-[90%]">
+          <p className="text-[16px] text-center">
             Empowering Healthy Futures, One School at a Time!
           </p>
           <div className="flex items-center justify-center bg-teal-500 px-4 py-2 rounded-md cursor-pointer">
             <a href="/contact" className="decoration-none">
               Contact Us
             </a>{" "}
-            <IoMdArrowForward className="ml-2" />
+            <IoMdArrowForward className="ml-2" />q
           </div>
         </div>
       </div>
