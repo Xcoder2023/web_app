12664dbf11b660b06018131ca8c39400de4e6a26
adebabs-tec <ruqoyatomotayo@gmail.com>
2025-01-11 20:51:41 +0100

    startd adding about section to landing page

---
+++ src/Components/landingPage/aboutSection/page.jsx
@@ -0,0 +1,14 @@
+const AboutSection = () => {
+  return (
+    <div className="px-4 md:px-8 lg:px-24 w-full">
+      <div className="flex items-center justify-between mx-auto gap-10">
+        <div className="flex items-center justify-between gap-10">
+          <div className="flex flex-col w-full q"></div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
